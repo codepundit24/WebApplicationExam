ccf49a5e5f8c7cec0c0d4a5f8c44fc3b66a79a25
codepundit24 <sharmaanurag897@gmail.com>
2022-02-19 17:58:01 +0530

    Add project files.

---
+++ Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationExam.Models
+{
+    public class Class1
+    {
+        public string Name { get; set;}
+        public string center { get; set; }
+        public int rollno { get; set; }
+    }
+}
---
+++ Models/NewClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationExam.Models
+{
+    public class NewClass
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public Decimal Rate { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
